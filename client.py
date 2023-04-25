d26a80fc87594cef3690a3054cf808fd554c0264
youlc1021 <92800216+youlc1021@users.noreply.github.com>
2023-04-25 18:49:47 +0800

    Add files via upload

--- client.py
+++ client.py
@@ -1,18 +1,20 @@
 from docarray import  Document
 from jina import Client, DocumentArray
+import numpy as np
+import os
+import glob
 
+img = DocumentArray.from_files('./img/*.jpg')
+for i in img:
+    i = i.load_uri_to_image_tensor()
 
-while True:
-    da = DocumentArray(Document(text=input('enter prompt>')))
-    img_list = input("enter images>").split(' ')
-    da.extend([Document(uri=img).load_uri_to_image_tensor() for img in img_list])
-    client = Client(port=55288)
-    service = input('enter 0 for scores and 1 for rank>')
-    if service == '0':
-        response = client.post(on='/score', inputs=da)
-        print('score:', response[0].text)
-    elif service == '1':
-        response = client.post(on='/rank', inputs=da)
-        print('uri:', response[0].text)
-        print('rank:', response[2].text)
-        print('score:', response[1].text)
+da = DocumentArray(
+    [Document(text='This is a photo of a pumpkin',matches=img)]
+)
+client = Client(port=50847)
+response = client.post(on='/rank', inputs=da)
+print('rank:', response[0].tensor)
+print('rewards:', response[1].tensor)
+
+# c = np.stack((response[0].tensor, response[1].tensor),axis=1)
+# print(c)
