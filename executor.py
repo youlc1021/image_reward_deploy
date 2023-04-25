88cecebe54823be0d599c9c7c47b460423cd043d
youlc1021 <92800216+youlc1021@users.noreply.github.com>
2023-04-25 18:43:44 +0800

    Add files via upload

--- executor.py
+++ executor.py
@@ -4,52 +4,32 @@ import ImageReward as RM
 import torch
 from jina import Executor, requests, DocumentArray
 from docarray import Document
+from PIL import Image
+import numpy as np
 
 
 class TextReward(Executor):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.model = RM.load("ImageReward-v1.0")
-        self.model_score = self.model.score
-        self.model_rank = self.model.inference_rank
-
-    def save_images(self, img_list):
-        for i in img_list:
-            i.save_image_tensor_to_file('./saved/' + i.uri.split('/')[-1])
-
-    @requests(on='/score')
-    def score(self, docs: DocumentArray, **kwargs):
-        # can't access through client local uri
-        # save images to server
-        self.save_images(docs[1:])
-        # uri from server
-        img_list = []
-        for i in docs[1:]:
-            img_list.append('./saved/'+ i.uri.split('/')[-1])
-        # input text and uri to model and get a list
-        with torch.no_grad():
-            scores = self.model_score(docs[0].text, img_list)
-        # return result as da
-        result = DocumentArray(Document(text=str(scores)))
-        # delete saved images
-        for i in docs[1:]:
-            os.remove('./saved/' + i.uri.split('/')[-1])
-        return result
 
     @requests(on='/rank')
     def rank(self, docs: DocumentArray, **kwargs):
-        self.save_images(docs[1:])
-        img_list = []
-        for i in docs[1:]:
-            img_list.append('./saved/' + i.uri.split('/')[-1])
-        ranking, rewards = self._rank(docs[0].text, img_list)
-        result = DocumentArray(Document(text=str(docs[1:,'uri'])))
-        result.extend([Document(text=str(rewards)),Document(text=str(ranking))])
-        for i in docs[1:]:
-            os.remove('./saved/' + i.uri.split('/')[-1])
-        return result
+        # for d in docs
+        ranking, rewards = self._rank(docs)
+        ranking = np.array(ranking)
+        rewards = np.array(rewards)
+        return DocumentArray([Document(tensor=ranking),Document(tensor=rewards)])
 
-    def _rank(self, prompt, img_list):
+    def _rank(self, docs:DocumentArray):
+        rnk, rwd = [], []
+        img = []
         with torch.no_grad():
-            ranking, rewards = self.model_rank(prompt, img_list)
+            for i in docs['@m']:
+                i = Image.fromarray(i.tensor)
+                img.append(i)
+            print(docs['@r'][0].text)
+            ranking, rewards = self.model.inference_rank(docs['@r'][0].text, img)
+            rnk.append(ranking)
+            rwd.append(rewards)
         return ranking, rewards
